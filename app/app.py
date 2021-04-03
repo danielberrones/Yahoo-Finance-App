d966c28ad635675b6d61d56ecda2317decb0d720
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-04-03 18:28:29 -0500

    Update

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 import yfinance as yf
 
 
+
 # GOOG = Google
 # APPL = Apple
 # DASH = DoorDash
 # NFLX = Netflix
-
+# TSLA = Tesla
 userTicker = input("Enter ticker symbol: ")
 userTicker = yf.Ticker(userTicker)
 for k,v in userTicker.info.items():
