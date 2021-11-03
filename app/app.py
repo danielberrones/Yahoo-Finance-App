165b7abde636b7e147106ff5c8e8710040f42044
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-11-03 12:45:16 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,20 +1,16 @@
 import yfinance as yf
 
-
-
 # GOOG = Google
 # APPL = Apple
 # DASH = DoorDash
 # NFLX = Netflix
 # TSLA = Tesla
+
 userTicker = input("Enter ticker symbol: ")
 userTicker = yf.Ticker(userTicker)
 for k,v in userTicker.info.items():
     if k == "longBusinessSummary":
-        # print(k)
-    
-        # print(f'KEY:{k}, VALUE:{v}')
-        with open('writeMe.txt','w') as f:
+        with open('longBusinessSummary.txt','w') as f:
                 f.write(str(v))
 
 
